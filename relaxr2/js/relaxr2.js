a1bb4f0cd15d787564d9fb9f63190539353b359e
Justin <justinpatrickmcgee@gmail.com>
2018-09-16 16:47:08 -0700

    First changes to the interactivity for the Relaxr site

---
+++ relaxr2/js/relaxr2.js
@@ -0,0 +1,8 @@
+// Prevent a form Submission
+
+// Delay js from loading until DOM has loaded
+
+// Select the appropriate CSS selector upon a user's click
+
+// "Read More" on primary column
+// Have <p> slide down with "Read Less" link
